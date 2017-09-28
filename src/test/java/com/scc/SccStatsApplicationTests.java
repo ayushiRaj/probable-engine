9919ceb02201897bad87500339e3e94f02b85d94
Jayendra Vikram Singh <jayendra155@hotmail.com>
2017-09-28 17:47:22 +0530

    Added projection

--- src/test/java/com/scc/SccStatsApplicationTests.java
+++ src/test/java/com/scc/SccStatsApplicationTests.java
@@ -1,16 +1,38 @@
 package com.scc;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.scc.controller.PlayerController;
+import com.scc.data.repository.PlayerRepository;
+import com.scc.model.Player;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Profile("dev")
 public class SccStatsApplicationTests {
+
+	@Autowired
+	private PlayerController controller;
+
+	@Autowired
+	private PlayerRepository repo;
 
 	@Test
 	public void contextLoads() {
+		Map<String, String> map = new HashMap<>();
+		map.put("attribute", "BOWLER");
+		map.put("name", "david");
+		controller.addANewPlayer(map, null);
+//		repo.findAllPlayerNames().stream().forEach(System.out::println);
+		
 	}
 
 }
